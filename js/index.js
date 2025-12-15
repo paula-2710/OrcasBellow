72ee1f31cdf76f5d18d42e3d86f7db4ab3e30e04
paula-2710 <paula_2710@hotmail.com>
2025-12-16 00:47:38 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -15,4 +15,25 @@ document.addEventListener("DOMContentLoaded", () => {
     }, observerOptions);
 
     sections.forEach(section => observer.observe(section));
+
+    // === SWIPER CARRUSEL ===
+    new Swiper(".swiper-container", {
+        slidesPerView: 3,       // cuántas se ven
+        spaceBetween: 2,        // muy poquito espacio
+        centeredSlides: true,   // siempre centradas
+        loop: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        breakpoints: {
+            1024: { slidesPerView: 3, spaceBetween: 2 },
+            768:  { slidesPerView: 2, spaceBetween: 2 },
+            480:  { slidesPerView: 1, spaceBetween: 2 }
+        },
+    });
 });
